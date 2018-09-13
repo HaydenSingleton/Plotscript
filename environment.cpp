b9441bd13cb3da3c0e3824debe0d6b2ca4126201
Hayden Singleton <hgs@vt.edu>
2018-09-13 18:26:33 -0400

    Changed a variable name

--- environment.cpp
+++ environment.cpp
@@ -356,8 +356,8 @@ Expression conj(const std::vector<Expression> & args) {
 
 const double PI = std::atan2(0, -1);
 const double EXP = std::exp(1);
-const std::complex<double> I (0.0,1.0);
-const std::complex<double> minusI (0.0,-1.0);
+const std::complex<double> IMG (0.0,1.0);
+const std::complex<double> NEG_IMG (0.0,-1.0);
 
 Environment::Environment(){
 
@@ -441,10 +441,10 @@ void Environment::reset(){
   envmap.emplace("e", EnvResult(ExpressionType, Expression(EXP)));
 
   // Built-In value of i
-  envmap.emplace("I", EnvResult(ExpressionType, Expression(I)));
+  envmap.emplace("I", EnvResult(ExpressionType, Expression(IMG)));
 
   // Built-In value of -i
-  envmap.emplace("-I", EnvResult(ExpressionType, Expression(minusI)));
+  envmap.emplace("-I", EnvResult(ExpressionType, Expression(NEG_IMG)));
 
   // Procedure: add;
   envmap.emplace("+", EnvResult(ProcedureType, add));
