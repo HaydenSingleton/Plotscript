ef757e261b0e497376d27e1359eab527057d0917
Hayden Singleton <hgs@vt.edu>
2018-10-16 16:49:34 -0400

    Added parsing of expressions, still need output

--- notebook_app.hpp
+++ notebook_app.hpp
@@ -7,11 +7,21 @@
 #include "input_widget.hpp"
 #include "output_widget.hpp"
 
+#include "interpreter.hpp"
+#include "semantic_error.hpp"
+#include "startup_config.hpp"
+#include <string>
+#include <fstream>
+#include <sstream>
+
+
+
+
 class NotebookApp : public QWidget{
     Q_OBJECT
 
     public:
-        explicit NotebookApp(QWidget *parent = nullptr) {
+        explicit NotebookApp(QWidget *parent = 0) : QWidget(parent) {
             setObjectName("notebook");
             in = new InputWidget(this); //child widgets of notebook
             out = new OutputWidget(this);
@@ -21,11 +31,56 @@ class NotebookApp : public QWidget{
             layout->addWidget(out);
             setLayout(layout);
 
+            // connect input to this notebook for evaluating
+            QObject::connect(in, SIGNAL(send_input(QString)), this, SLOT(catch_input(QString)));
+
+            // sent evaluated expressions to output widget
+            QObject::connect(this, SIGNAL(send_result(Expression)), out, SLOT(catch_result(Expression)));
+
+            // send any errors to output widget with a different mechanism
+            QObject::connect(this, SIGNAL(failure(int, std::string)), out, SLOT(catch_failure(int, std::string)));
+
+            //startup procedure
+            std::ifstream startip_str(STARTUP_FILE);
+            if(!mrInterpret.parseStream(startip_str)){
+                emit failure(EXIT_FAILURE, "Invalid Program. Could not parse.");
+            }
+            else{
+                try{
+                    mrInterpret.evaluate();
+                }
+                catch(const SemanticError & ex){
+                    emit failure(EXIT_FAILURE, ex.what());
+                }
+            }
         };
+
+    private slots:
+        void catch_input(QString r){
+            std::istringstream expression(r.toStdString());
+            if(!mrInterpret.parseStream(expression)){
+                emit failure(0, "Invalid Expression. Could not parse.");
+            }
+            else{
+                try{
+                    Expression exp = mrInterpret.evaluate();
+                    emit send_result(exp);
+                    std::cout << exp << std::endl;
+                }
+                catch(const SemanticError & ex){
+                    emit failure(0, ex.what());
+                }
+            }
+        };
+
+    signals:
+        void send_result(Expression exp);
+        void failure(int code, std::string message);
 
     private:
         InputWidget *in;
         OutputWidget *out;
+        Interpreter mrInterpret;
 };
 
 #endif
