e7a14f4d722ab4bf82c787b932d43e982b8900e5
Hayden Singleton <hgs@vt.edu>
2018-10-15 21:00:50 -0400

    Changed class structure for notebook app

--- notebook.cpp
+++ notebook.cpp
@@ -1,16 +1,11 @@
 #include <QApplication>
-#include <QWidget>
-#include "myWidgets.hpp"
+#include "notebook_app.hpp"
 
 int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
 
   NotebookApp widget;
-  widget.setObjectName("NotebookApp");
-
-
-
   widget.show();
 
   return app.exec();
